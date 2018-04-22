00f3663328a4b6629f56d9f52da4bc26b3bb854f
Tanvi Singh <ts377@njit.edu>
2018-04-21 21:01:27 -0400

    Added list of questions on homepage and a pager

--- database/seeds/AnswersTableSeeder.php
+++ database/seeds/AnswersTableSeeder.php
@@ -12,14 +12,15 @@ class AnswersTableSeeder extends Seeder
     public function run()
     {
         $users = App\User::inRandomOrder();
-        $users->each(function ($user){
-            $question = App\Question::inRandomOrder()->first();
-            $answer = factory(\App\Answer::class)->make();
-            $answer->user()->associate($user);
-            $answer->question()->associate($question);
-            $answer->save();
-        });
-    }
+
+            $users->each(function ($user) {
+                $question = App\Question::inRandomOrder()->first();
+                $answer = factory(\App\Answer::class)->make();
+                $answer->user()->associate($user);
+                $answer->question()->associate($question);
+                $answer->save();
+            });
+        }
 
 
 }
